82d46a86a8663d787a8de46a6b38704506d24822
MateuszRym <149682572+MateuszRym@users.noreply.github.com>
2024-11-19 00:52:13 +0100

    Adding basic functions

--- pk-project/pid.cpp
+++ pk-project/pid.cpp
@@ -1,6 +1,38 @@
 #include "pid.h"
 
-pid::pid(int tempK, int tempTI, int tempTD) : k{tempK}, tI{tempTI}, tD{tempTD}
-{
+pid::pid(int tempK, int tempTI, int tempTD) : k{tempK}, tI{tempTI}, tD{tempTD}{
+
+}
+
+pid::~pid(){
+
+}
+
+void pid::ustawK(int tempK){
+    k = tempK;
+}
+
+void pid::ustawTI(int tempTI){
+    tI = tempTI;
+}
+
+void pid::ustawTD(int tempTD){
+    tD = tempTD;
+}
+
+void pid::resetujPamiecCalk(){
+    pamiecCalk = 0;
+}
+
+void pid::resetujPamiecRozn(){
+    pamiecRozn = 0;
+}
+
+void pid::resetujPamiec(){
+    resetujPamiecCalk();
+    resetujPamiecRozn();
+}
+
+float pid::symulujKrokPID(float eI){
 
 }
--- pk-project/pid.h
+++ pk-project/pid.h
@@ -6,8 +6,18 @@ class pid
     int k;
     int tI;
     int tD;
+    float pamiecCalk;
+    float pamiecRozn;
 public:
     pid(int tempK, int tempTI, int tempTD);
+    ~pid();
+    void ustawK(int tempK);
+    void ustawTI(int tempTI);
+    void ustawTD(int tempTD);
+    void resetujPamiecCalk();
+    void resetujPamiecRozn();
+    void resetujPamiec();
+    float symulujKrokPID(float eI);
 };
 
 #endif // PID_H
